7766cc799e7f450588c54a4be40c7f3aaeebcccf
XtianFreolo <f60@umbc.edu>
2025-04-16 13:31:48 -0400

    Task 3

--- Starter Code/Arrow functions SpaceCraft.js
+++ Starter Code/Arrow functions SpaceCraft.js
@@ -14,6 +14,10 @@ console.log(scanForLife());
 /* Task 3: Implicit Return with Objects: Log Coordinates */
 // TODO: Write an arrow function named `currentCoordinates` that returns an object with properties `x`, `y`, and `z`, representing coordinates in space. Use implicit return. Print the returned object from `currentCoordinates`.
 
+const currentCooedinates = () => ({ x:0, y:0, z:0 });
+console.log(currentCoordionates());
+
+
 /* Task 4: Understanding `this`: Message from Home Base */
 // TODO: Inside an object named `spacecraft`, create a method named `receiveMessage` using arrow function syntax. This method should log `"Message received: "` followed by a message it receives as a parameter. Directly call `receiveMessage` within `spacecraft` and observe. Observe and explain the behavior of `this` in this context as a comment.
 /*
